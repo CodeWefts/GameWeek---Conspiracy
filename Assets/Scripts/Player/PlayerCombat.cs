9fdf36215830a66be3a20d10301e93927de66f5f
Dorian <d.husson@student.isartdigital.com>
2024-09-10 16:48:34 +0200

    [ADD] Melee attack

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    [SerializeField] private float m_AttackRange = 1f;
+    [SerializeField] private float m_AttackDamage = 1f;
+
+    [SerializeField] private LayerMask m_EnemyLayer;
+
+    private string m_MeleeAttack = "MeleeAttack";
+
+    private Collider[] m_EnemiesHit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,26 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown(m_MeleeAttack))
+        {
+            MeleeAttack();
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        m_EnemiesHit = Physics.OverlapSphere(transform.position, m_AttackRange, m_EnemyLayer);
+
+        foreach (Collider lEnemy in m_EnemiesHit)
+        {
+            Debug.Log("Hit :"+ lEnemy.gameObject.name);
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, m_AttackRange);
     }
 }
