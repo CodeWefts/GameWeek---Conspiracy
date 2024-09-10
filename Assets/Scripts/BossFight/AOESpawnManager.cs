a8a0936640afffa34d051685813c09192de2329d
Morgane Dero <m.dero@student.isartdigital.com>
2024-09-10 18:16:18 +0200

    Random AOE Finish + AOE waves (but have to change it)

--- Assets/Scripts/BossFight/AOESpawnManager.cs
+++ Assets/Scripts/BossFight/AOESpawnManager.cs
@@ -2,74 +2,107 @@ using System;
 using System.Collections;
 using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Threading;
 using UnityEngine;
 
 public class AOESpawnManager : MonoBehaviour
 {
-    [SerializeField] public bool circle = true;
-    [SerializeField] public bool rectangle = false;
-    [SerializeField] public bool isBossAOEPhase = false;
+    // #######################  PUBLIC  #######################
+    // --------------------------------------------------------
+    [SerializeField] public bool isBossAOEPhase = true;
+    [SerializeField] public bool isRandomPhase = false;
 
     [SerializeField] public GameObject AOEZoneObject;
     [SerializeField] public GameObject AOEPhoneObject;
     [SerializeField] public GameObject PlayerCameraObject;
+    [SerializeField] public float time = 0.0f;
 
-    public double circleRadius = 0.0f;
-    public Vector3 rectangleSize;
-    double AOEZoneRadius;
-    public Vector3 AOEZoneSize;
+    [SerializeField] public double thetaValue = 5.0f * Math.PI / 6.0f;
+    [SerializeField] public double angleValue = 0.0f;
+
+    // ######################  PRIVATE  #######################
+    // --------------------------------------------------------
+    private bool isCoroutine = false;
+    private double circleRadius = 0.0f;
+    private Vector3 rectangleSize;
+    private double AOEZoneRadius;
+    private Vector3 AOEZoneSize;
 
     // Start is called before the first frame update
+    // ---------------------------------------------
     void Start()
     {
-        // Get the size of the map (circle or rectangle only)
-        if (rectangle)
-        {
-            rectangleSize = gameObject.transform.localScale;
-            AOEZoneSize = AOEZoneObject.transform.localScale;
-
-            // TODELETE
-            UnityEngine.Debug.Log("Rectangle Width: " + rectangleSize.x + " Rectangle Lenght: " + rectangleSize.y);
-        }
-        else
-        {
-            circleRadius = gameObject.transform.localScale.x;
-            AOEZoneRadius = AOEZoneObject.transform.localScale.x;
-
-            // TODELETE
-            UnityEngine.Debug.Log("Circle Radius: " + circleRadius);
-        }
+        // Get the size of the map
+        circleRadius = gameObject.transform.localScale.x / 2.0f;
+        AOEZoneRadius = AOEZoneObject.transform.localScale.x / 2.0f;
     }
 
-    Vector3 SetAOESpawnPosition()
+    // Set the random position of the AOE's Zone
+    // -----------------------------------------
+    Vector3 SetAOERandomSpawnPosition()
     {
         Vector3 AOEPosition;
 
-        if (rectangle)
-        {
-            // TODO : Set the AOE's spawning position in the rectangle
-            AOEPosition = new Vector3(UnityEngine.Random.Range(-rectangleSize.x, rectangleSize.x), UnityEngine.Random.Range(-rectangleSize.y, rectangleSize.y), rectangleSize.z);
-        }
-        else
-        {
-            // TODO : the value "2.0f" is here because de sphere is scale to 20 and the AOE is scale to 1.5......
-            // TODO : the value "(AOEZoneObject / 2.0f)" is here because we don't want to outreach the radius of the map
-            double radius = (circleRadius / 2.0f) * Math.Sqrt(UnityEngine.Random.Range(0.0f, 1.0f)) - (AOEZoneObject.transform.localScale.x / 2.0f);
-            double theta = UnityEngine.Random.Range(0.0f, 1.0f) * 2 * Math.PI;
+        double radius = circleRadius * Math.Sqrt(UnityEngine.Random.Range(0.0f, 1.0f)) - AOEZoneObject.transform.localScale.x;
+        double theta = UnityEngine.Random.Range(0.0f, 1.0f) * 2 * Math.PI;
 
-            double positionX = gameObject.transform.position.x + radius * Math.Cos(theta);
-            double positionZ = gameObject.transform.position.z + radius * Math.Sin(theta);
+        double positionX = gameObject.transform.position.x + radius * Math.Cos(theta);
+        double positionZ = gameObject.transform.position.z + radius * Math.Sin(theta);
 
-            // Set the position of the AOE
-            AOEPosition = new Vector3((float)positionX, 0.01f, (float)positionZ);
-        }
+        // Set the position of the AOE
+        AOEPosition = new Vector3((float)positionX, 0.01f, (float)positionZ);
 
         return AOEPosition;
     }
 
-    void AOESpawn()
+    // Set the wave position of the AOE's Zone
+    // ---------------------------------------
+    Vector3 SetAOEWaveSpawnPosition()
     {
-        Vector3 AOEPosition = SetAOESpawnPosition();
+        Vector3 AOEPosition;
+
+        // The value "AOEZoneRadius" is here because we don't want to outreach the radius of the map
+        double radius = circleRadius - AOEZoneRadius;
+
+        double positionX = radius * Math.Cos(thetaValue);
+        double positionZ = radius * Math.Sin(thetaValue + angleValue);
+
+        // Set the position of the AOE
+        AOEPosition = new Vector3((float)positionX, 0.01f, (float)positionZ);
+
+        return AOEPosition;
+    }
+
+    // Spawn the AOE's Zone
+    // --------------------
+    void AOESpawn(bool isRandom)
+    {
+        Vector3 AOEPosition = SetAOERandomSpawnPosition();
+
+        if (isRandom)
+        {
+            AOEPosition = SetAOERandomSpawnPosition();
+        }
+        else
+        {
+            AOEPosition = SetAOEWaveSpawnPosition();
+
+            // Row
+            double calcul = (Math.PI - thetaValue) * 2.0f;
+            if (angleValue < calcul)
+            {
+                angleValue += 0.25f;
+            }
+            else
+            {
+                angleValue = 0.0f;
+                thetaValue -= 0.25f;
+                if (thetaValue < (Math.PI / 6.0f))
+                {
+                    isBossAOEPhase = false;
+                }
+            }
+        }
 
         // Spawning of the AOE's Zone on the floor
         GameObject newAOEZoneObject = Instantiate(AOEZoneObject);
@@ -79,23 +112,28 @@ public class AOESpawnManager : MonoBehaviour
         Vector3 phoneHeight = new Vector3(0.0f, PlayerCameraObject.transform.position.y + AOEPhoneObject.transform.localScale.y, 0.0f);
         GameObject newAOEPhoneObject = Instantiate(AOEPhoneObject);
         newAOEPhoneObject.transform.position = AOEPosition + phoneHeight;
+    }
 
-        // TODELETE
-        UnityEngine.Debug.Log("AOEPosition: " + AOEPosition);
+    // Timer for the AOE's Zone
+    // ------------------------
+    private IEnumerator Timer()
+    {
+        while (isBossAOEPhase)
+        {
+            AOESpawn(isRandomPhase);
+            yield return new WaitForSeconds(0.2f);
+        }
+        isCoroutine = false;
     }
 
     // Update is called once per frame
+    // -------------------------------
     void Update()
     {
-        int bossSpawnAOE = 0;
-        if (isBossAOEPhase)
+        if (isBossAOEPhase && !isCoroutine)
         {
-            AOESpawn();
-            bossSpawnAOE++;
-
-            // TODO : Set a timer ( time of the animation + spawning objects ) in a row of 3 then pass the boolean to false.
-            isBossAOEPhase = false;
-            bossSpawnAOE = 0;
+            StartCoroutine(Timer());
+            isCoroutine = true;
         }
     }
 }
