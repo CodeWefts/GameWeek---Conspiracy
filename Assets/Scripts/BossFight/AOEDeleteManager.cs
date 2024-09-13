ccbab2b766d184393a94ed3217afdb15c98033da
Morgane Dero <m.dero@student.isartdigital.com>
2024-09-13 09:43:07 +0200

    Adding hit sound and solve boss damage error

--- Assets/Scripts/BossFight/AOEDeleteManager.cs
+++ Assets/Scripts/BossFight/AOEDeleteManager.cs
@@ -14,18 +14,13 @@ public class AOEDeleteManager : MonoBehaviour
         }
         if (other.gameObject.tag == "AOEZone")
         {
-            //GameObject destroyEffect = Instantiate(destroyEffectObject);
-            //destroyEffect.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - gameObject.localScale / 2.0f, gameObject..transform.position.z);
             Destroy(other.gameObject);
             Destroy(gameObject, 0.1f);
             isPhoneDestroyed = true;
         }
         if (other.gameObject.layer == 3 && other.gameObject.TryGetComponent(out PlayerCombat playerScript) && isPhoneDestroyed)
         {
-            //if (playerScript.PlayerMovement.IsPlayerVulnerable)
-            //{
             playerScript.DamageTaken(PlayerDamage);
-            //}
         }
     }
 
